c5df82399b3ca8e8aff9eb4110d866dad6d2f4f4
Ashbee Morgado <ashbee.morgado@icloud.com>
2021-03-04 13:53:57 +0800

    .

--- app/BulkDisbursement.php
+++ app/BulkDisbursement.php
@@ -7,7 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class BulkDisbursement extends Model
 {
     protected $fillable = ['bulk_disbursement_id','loan_account_id','office_id','disbursement_date'];
-
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'disbursement_date'
+    ];
     public function loanAccount(){
         return $this->belongsTo(LoanAccount::class);
     }
